5b651824e8265b0c294ba2a6182a0473872d73fa
adambinch <abinch@sagarobotics.com>
2022-12-06 12:47:58 +0000

    Client node stays active (rospy spin) with on node shutdown that calls the kills monitors service with tags used (behaviour set with arg).

--- scripts/sentor_client.py
+++ scripts/sentor_client.py
@@ -9,52 +9,61 @@ Created on Fri Jul  1 09:25:27 2022
 import rospy, sys, argparse
 from sentor.srv import Client, ClientRequest
 
+modes = {}
+modes["load"] = "/sentor/load_monitors"
+modes["stop"] = "/sentor/stop_monitors"
+modes["start"] = "/sentor/start_monitors"
+modes["kill"] = "/sentor/kill_monitors"
 
-def usage():
-    print("\nLoads/stops/starts topic monitors:")
-    print("\nFor loading monitors:")
-    print("\t rosrun sentor sentor_client.py load CONFIG TOPIC_TAG_1,TOPIC_TAG_2,TOPIC_TAG_N")
-    print("\nFor stopping monitors:")
-    print("\t rosrun sentor sentor_client.py stop TOPIC_TAG_1,TOPIC_TAG_2,TOPIC_TAG_N")
-    print("\nFor starting monitors:")
-    print("\t rosrun sentor sentor_client.py start TOPIC_TAG_1,TOPIC_TAG_2,TOPIC_TAG_N")
-    print("\nFor killing monitors:")
-    print("\t rosrun sentor sentor_client.py kill TOPIC_TAG_1,TOPIC_TAG_2,TOPIC_TAG_N")
-    print("\nTopic tags are optional args used to load/stop/start/kill specific monitors")
-    print("\n\n")
+
+class sentor_client(object):
+
+
+    def __init__(self, mode, config, tags, spin):
+
+        self.tags = tags
+
+        self.call_service(modes[mode], config, tags)
+
+        if spin and mode == "load":
+            rospy.on_shutdown(self._on_node_shutdown)
+            rospy.spin()
+
+
+    def call_service(self, name, config, tags):
+
+        print("Calling service...")
+        print("\tname: {}".format(name))
+        print("\tconfig: {}".format(config))
+        print("\ttags: {}".format(tags))
     
-    
-def sentor_client(name, config, tags):
-    
-    rospy.wait_for_service(name, timeout=5.0)
-    try:
-        s = rospy.ServiceProxy(name, Client)
-        req = ClientRequest()
-        req.config = config
-        req.topic_tags = tags
-        resp = s.call(req)
-        rospy.loginfo(resp)
-    except rospy.ServiceException as e:
-        print("Service call failed: {}".format(e))
+        rospy.wait_for_service(name, timeout=10.0)
+        try:
+            s = rospy.ServiceProxy(name, Client)
+            req = ClientRequest()
+            req.config = config
+            req.topic_tags = tags
+            resp = s.call(req)
+            rospy.loginfo(resp)
+        except rospy.ServiceException as e:
+            print("Service call failed: {}".format(e))
+
+
+    def _on_node_shutdown(self):
+            self.call_service(modes["kill"], "", self.tags)
 ###################################################################################################################
     
     
 ###################################################################################################################
 if __name__ == "__main__":
 
-    modes = {}
-    modes["load"] = "/sentor/load_monitors"
-    modes["stop"] = "/sentor/stop_monitors"
-    modes["start"] = "/sentor/start_monitors"
-    modes["kill"] = "/sentor/kill_monitors"
-
     parser = argparse.ArgumentParser()
     parser.add_argument("mode", type=str,
-                        help="(str) load, stop, start or kill. Required.")
+                        help="(str) load, stop, start or kill (monitors). Required.")
     parser.add_argument("--config", type=str, default="",
                         help="(str) sentor config file. Required if mode=load.")
-    parser.add_argument("--tags", type=str, default="",
-                        help="(str) topic tags used to load/stop/start/kill individual monitors. Optional.")
+    parser.add_argument("--topic_tags", type=str, default="",
+                        help="(str) topic tags used to load/stop/start/kill individual monitors. Separate multiple topic tags using commas. Optional.")
     parser.add_argument("--spin", type=lambda x: (str(x).lower() == "true"), default=False, 
                         help="(bool) Spin the client and kill loaded monitors on shutdown. Default=False")
     
@@ -66,41 +75,5 @@ if __name__ == "__main__":
         rospy.logerr("--config CONFIG  (str) sentor config file. Required if mode=load.")
         sys.exit(1)
 
-    sentor_client(modes[args.mode], args.config, args.tags)
-
-
-
-
-
-       # if '-h' in sys.argv or '--help' in sys.argv or len(sys.argv) < 2:
-    #     usage()
-    #     sys.exit(1)
-    # else:
-    #     mode = ""
-    #     config = ""
-    #     tags = ""
-    #     if sys.argv[1] == "load":
-    #         mode = "/sentor/load_monitors"
-    #         if len(sys.argv) > 2:
-    #             config = sys.argv[2]
-    #             if len(sys.argv) > 3:
-    #                 tags = sys.argv[3]
-    #         else:
-    #             usage()
-    #             sys.exit(1)
-    #     elif sys.argv[1] == "stop":
-    #         mode = "/sentor/stop_monitors"
-    #         if len(sys.argv) > 2:
-    #             tags = sys.argv[2]
-    #     elif sys.argv[1] == "start":
-    #         mode = "/sentor/start_monitors"
-    #         if len(sys.argv) > 2:
-    #             tags = sys.argv[2]
-    #     elif sys.argv[1] == "kill":
-    #         mode = "/sentor/kill_monitors"
-    #         if len(sys.argv) > 2:
-    #             tags = sys.argv[2]
-    #     else:
-    #         usage()
-    #         sys.exit(1)
+    sentor_client(args.mode, args.config, args.topic_tags, args.spin)
 ###################################################################################################################
